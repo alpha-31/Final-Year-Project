34164d5c535460d87338a444337efa2f6235a692
Sheikh Abdul Wahid <sheikhabdul.bcss19@iba-suk.edu.pk>
2022-08-31 04:05:41 +0500

    Initial Web-App

---
+++ sky-net/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p >
+          <marquee >
+           Coming soon A Web app  
+          </marquee>>  
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
